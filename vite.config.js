b523d803d3690193b087526550e3ce392071228d
[Mohammed Alfez] <[alfez637@gmail.com]>
2024-09-22 18:55:35 +0530

    Fourth commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Food-website/',
+  base: '/Food-Website/',
   build: {
     chunkSizeWarningLimit: 1000,  
   },
